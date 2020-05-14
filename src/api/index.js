a2927e72a4ae72547bb4e1d8d1958335c2a584a8
Kirankumar Pyati <kirankumar.pyati@sap.com>
2020-05-14 22:15:57 +0530

    Post API of subrequest creation integration
    Signed-off-by: Kirankumar Pyati <kirankumarp92@gmail.com>

--- src/api/index.js
+++ src/api/index.js
@@ -127,23 +127,8 @@ async function getRequestForHelpDetail(requestID) {
 }
 
 async function saveHelpRequestForHelpUpdate(formData) {
-  const res = await server.post("/subrequest", formData);
+  const res = await server.post("/request/subrequest", formData);
   return res || null;
-
-
-/**
- * This function is used by the NGO form search to search data in the ngos collection
- * @param {*} params The parameters on which search should be done
- * @return Returns the search results
- */
-async function searchNgoForm(params) {
-  const res = await server.post(`/ngo/search`, params);
-  return res.data.data || [];
-}
-
-async function exportNgoForm(params) {
-  const res = await server.post(`/ngo/export/`, params);
-  return res.data.data || [];
 }
 
 export default {
@@ -162,10 +147,7 @@ export default {
   exportKind,
   exportRequests,
   updateStatus,
-  searchNgoForm,
   saveNgoForm,
   getRequestForHelpDetail,
   saveHelpRequestForHelpUpdate,
-  exportNgoForm,
-  saveNgoForm,
 };
